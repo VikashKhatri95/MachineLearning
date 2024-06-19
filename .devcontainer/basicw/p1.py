1d9ad8f17bcd6e9b28ff9c9025b07ba4f23b30a3
Vikash Khatri <102739330+VikashKhatri95@users.noreply.github.com>
2024-06-19 13:22:43 +0000

    initial commit

---
+++ .devcontainer/basicw/p1.py
@@ -0,0 +1,15 @@
+# list are ordered mutable collection of items.
+lst=[]
+# print(type(lst))
+
+names=["Vikash","krish","Vijay"]
+# print(names)
+mixed_list=[1,"Hello",3.14,True]
+# print(mixed_list)
+
+#Accesing list elements
+fruits=["apple","banana","cherry","kiwi"]
+# print(fruits[1])#["banana"]
+# print(fruits[:2])#["apple","banana"]
+#print(fruits)#["apple","banana","cherry","kiwi"]
+# print(fruits[1:-1])#['banana', 'cherry']
